2addcab911f47138b519fc03503ba9e166eb0b2a
Andrian Yarotskyi <andrian.yarotskyi@stormotion.io>
2025-07-18 19:55:17 +0200

    dockerize the whole app

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,12 +5,10 @@ use std::{net::SocketAddr, str::FromStr};
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv::dotenv().ok();
-
     let server = HttpServer::new(HttpServerConfig {
         addr: SocketAddr::from_str(&format!(
             "{}:{}",
-            std::env::var("IP").expect("IP must be set"),
+            std::env::var("HOST").expect("HOST must be set"),
             std::env::var("PORT").expect("PORT must be set")
         ))
         .unwrap(),
