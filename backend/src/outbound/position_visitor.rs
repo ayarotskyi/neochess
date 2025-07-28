4cd4734a6af2b2f3a08941f763758bcd7fd5ecf8
Andrian Yarotskyi <a.yarotskyi@gmail.com>
2025-07-28 13:57:12 +0200

    add tests for position visitor

--- backend/src/outbound/position_visitor.rs
+++ backend/src/outbound/position_visitor.rs
@@ -48,9 +48,13 @@ impl Visitor for PositionVisitor {
                     Ok(chess) => chess,
                     Err(_) => return ControlFlow::Break(Err(InvalidPgnError(self.pgn.clone()))),
                 };
-                movetext.result.push(Fen::new_unchecked(
-                    &movetext.chess.board().board_fen().to_string(),
-                ));
+                let next_fen = shakmaty::fen::Fen::try_from_setup(
+                    movetext.chess.to_setup(shakmaty::EnPassantMode::Always),
+                );
+                match next_fen {
+                    Ok(fen) => movetext.result.push(fen.into()),
+                    Err(_) => return ControlFlow::Break(Err(InvalidPgnError(self.pgn.clone()))),
+                };
                 ControlFlow::Continue(())
             }
             _ => ControlFlow::Break(Err(InvalidPgnError(self.pgn.clone()))),
@@ -66,5 +70,91 @@ impl Visitor for PositionVisitor {
 
     fn end_game(&mut self, movetext: Self::Movetext) -> Self::Output {
         Ok(movetext.result)
+    }
+}
+
+impl From<shakmaty::fen::Fen> for Fen {
+    fn from(value: shakmaty::fen::Fen) -> Self {
+        Self::new_unchecked(&value.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    use pgn_reader::Reader;
+
+    use super::*;
+
+    #[test]
+    fn test_pgn_success() {
+        let pgn = r#"
+            [Event "?"]
+            [Site "?"]
+            [Date "????.??.??"]
+            [Round "?"]
+            [White "?"]
+            [Black "?"]
+            [Result "1-0"]
+            [Link "https://www.chess.com/terms/scholars-mate-chess"]
+                
+            1. e4 e5 2. Bc4 Nc6 3. Qh5 g6 {This is a great defensive move by Black.
+            It defends the weak f7-pawn, prepares to fianchetto the bishop, starts clearing
+            the way for the black king to castle, and also attacks the white queen.} 4. Qf3
+            {The queen retreats, but notice how it still aims at the weak f7-pawn.} 4... Bg7
+            {If Black is not careful, White can end the game in an instant.} 5. Qxf7# 1-0"#;
+
+        let expected_fens: Result<Vec<Fen>, InvalidPgnError> = Ok(vec![
+            Fen::new_unchecked("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"),
+            Fen::new_unchecked("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2"),
+            Fen::new_unchecked("rnbqkbnr/pppp1ppp/8/4p3/2B1P3/8/PPPP1PPP/RNBQK1NR b KQkq - 1 2"),
+            Fen::new_unchecked("r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/8/PPPP1PPP/RNBQK1NR w KQkq - 2 3"),
+            Fen::new_unchecked("r1bqkbnr/pppp1ppp/2n5/4p2Q/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 3 3"),
+            Fen::new_unchecked(
+                "r1bqkbnr/pppp1p1p/2n3p1/4p2Q/2B1P3/8/PPPP1PPP/RNB1K1NR w KQkq - 0 4",
+            ),
+            Fen::new_unchecked(
+                "r1bqkbnr/pppp1p1p/2n3p1/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR b KQkq - 1 4",
+            ),
+            Fen::new_unchecked(
+                "r1bqk1nr/pppp1pbp/2n3p1/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 2 5",
+            ),
+            Fen::new_unchecked(
+                "r1bqk1nr/pppp1Qbp/2n3p1/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 5",
+            ),
+        ]);
+
+        let mut reader = Reader::new(io::Cursor::new(pgn));
+
+        let actual_fens = reader
+            .read_game(&mut PositionVisitor::new(pgn.into()))
+            .unwrap_or(Some(Err(InvalidPgnError(pgn.into()))))
+            .unwrap_or(Err(InvalidPgnError(pgn.into())));
+
+        assert_eq!(actual_fens, expected_fens);
+    }
+
+    #[test]
+    fn test_pgn_failure() {
+        let pgn = r#"
+            [Event "?"]
+            [Site "?"]
+            [Date "????.??.??"]
+            [Round "?"]
+            [White "?"]
+            [Black "?"]
+            [Result "1-0"]
+
+            1. e4 e5 2. Bc4 Nc6 3. Qh5 g6 4. Qf3 5. Qxf7# 1-0"#;
+
+        let mut reader = Reader::new(io::Cursor::new(pgn));
+
+        let actual_fens = reader
+            .read_game(&mut PositionVisitor::new(pgn.into()))
+            .unwrap_or(Some(Err(InvalidPgnError(pgn.into()))))
+            .unwrap_or(Err(InvalidPgnError(pgn.into())));
+
+        assert!(actual_fens.is_err());
     }
 }
